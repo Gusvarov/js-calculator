e011a402997e501a22e24286ff5cf31484d8ab27
Gusvarov <nazikgta24@gmail.com>
2019-09-07 15:05:43 +0000

    My Code

--- js/index.js
+++ js/index.js
@@ -6,13 +6,16 @@ const multiply = document.querySelector('.multiply');
 const divide = document.querySelector('.divide');
 const equal = document.querySelector('.equal');
 const display = document.querySelector('.display');
-const clear = document.querySelector('.clear');
+let clear = document.querySelector('.clear');
+let back = document.querySelector('.back');
 
 nums.forEach( buttonNum => buttonNum.addEventListener('click', numPressed));
 function numPressed(event) {
     const num = event.target.innerText;
     display.value += num;
 }
+
+
 opers.forEach ( buttonOper => buttonOper.addEventListener('click', operPressed));
 
 function operPressed(event) {
@@ -21,4 +24,27 @@ function operPressed(event) {
 }
 
 equal.addEventListener('click', () => display.value = eval(display.value) );
+back.addEventListener('click', backs());
 
+ function backs(){
+    let exp = document.form.textview.value
+    document.form.textview.value = exp.substring(0,exp.length-1)
+}
+// clear.addEventListener('click', () => clear.form.value );
+
+
+
+
+
+
+
+// function one() {
+//     let backSpace = back.form.text.value;
+//     bakcSpace = backSpace.substring(0, backSpace.length - 1);
+// }
+
+
+
+// function numClear() {
+//     clear.form.value = '';
+// }
