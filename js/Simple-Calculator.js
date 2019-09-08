372a4396da9e2cde3c52eaec8c1230cf42ac6c5f
Gusvarov <54152415+Gusvarov@users.noreply.github.com>
2019-09-08 18:22:57 +0300

    Update Simple-Calculator.js

--- js/Simple-Calculator.js
+++ js/Simple-Calculator.js
@@ -1,1 +1,14 @@
+const firstNumber = document.querySelector('.first-number');
+const secondNumber = document.querySelector('.second-number');
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const multiply = document.querySelector('.multiply');
+const divide = document.querySelector('.divide');
+const result = document.querySelector('.result');
+
+
+plus.addEventListener('click', () => result.innerText = parseInt(firstNumber.value) + parseInt(secondNumber.value));
+minus.addEventListener('click', () => result.innerText = parseInt(firstNumber.value) - parseInt(secondNumber.value));
+multiply.addEventListener('click', () => result.innerText = parseInt(firstNumber.value) * parseInt(secondNumber.value));
+divide.addEventListener('click', () => result.innerText = parseInt(firstNumber.value) / parseInt(secondNumber.value));
 
