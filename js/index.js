5dc1d10e7383f36f726c20caf10c5c8e9af56b94
Gusvarov <nazikgta24@gmail.com>
2019-09-08 16:51:18 +0000

    My Code

--- js/index.js
+++ js/index.js
@@ -1,13 +1,9 @@
 const nums = document.querySelectorAll('.numbers button');
 const opers = document.querySelectorAll('.opers');
-const plus = document.querySelector('.plus');
-const minus = document.querySelector('.minus');
-const multiply = document.querySelector('.multiply');
-const divide = document.querySelector('.divide');
 const equal = document.querySelector('.equal');
 const display = document.querySelector('.display');
-let clear = document.querySelector('.clear');
-let back = document.querySelector('.back');
+const clear = document.querySelector('.clear');
+const back = document.querySelector('.back');
 
 nums.forEach( buttonNum => buttonNum.addEventListener('click', numPressed));
 function numPressed(event) {
@@ -15,53 +11,25 @@ function numPressed(event) {
     display.value += num;
 }
 
-
-// one-time event
-// onetime(document.getElementById("myelement"), "click", handler);
-
-// handler function
-// function handler(e) {
-// 	alert("You'll only see this once!");
-// }
-
 opers.forEach ( buttonOper => buttonOper.addEventListener('click', operPressed));
 
 function operPressed(event) {
-    const oper = event.target.innerText;
+    let oper = event.target.innerText;
     display.value += oper;
-    for(let i = 0; i < opers.length; i++){
-     if (display.value.includes(opers[i].innerText)) {
-         display.value += event.target.innerText;
-     }
+    for (let i = 0; i === opers.length; i++){
+        if (!display.value.includes(opers[i].innerText)) {
+            display.value += event.target.innerText;
+        }
     }
 }
+
 
 equal.addEventListener('click', () => display.value = eval(display.value) );
 
 clear.addEventListener('click', () => display.value = '' );
 
-
-
-back.addEventListener('click', backSpace());
-
- function backSpace(){
-    let exp = display.value;
-//    display.value = exp.substring(0,exp.length-1)
-    if ( exp.length > 0 ) {
-        exp = exp.substring(0, exp.length - 1);
-        display.value = exp;
-    }
-}
+back.addEventListener('click', () => display.value = display.value.substring(0, display.value.length - 1 ));
 
 
 
-// function one() {
-//     let backSpace = back.form.text.value;
-//     bakcSpace = backSpace.substring(0, backSpace.length - 1);
-// }
 
-
-
-// function numClear() {
-//     clear.form.value = '';
-// }
